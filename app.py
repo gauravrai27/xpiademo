d4e70d1da975988b4bb25c8ea83c598e19cd69a9
gauravrai27 <gaurav.techusa@gmail.com>
2025-06-13 19:25:47 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    user_text = request.args.get('text', '')
+    html = f"""
+    <html>
+        <head><title>Text Display</title></head>
+        <body>
+            <h1>User Input</h1>
+            <p>{user_text}</p>
+            <form method="get">
+                <input type="text" name="text" placeholder="Enter your text here">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+    </html>
+    """
+    return render_template_string(html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
